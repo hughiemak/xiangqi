8b764f19e39a971975eca83557c9bac117ac11e7
Hughie Mak <hymak@qbssystem.com>
2019-08-28 12:22:54 +0800

    Updates

---
+++ precache-manifest.db721438029dd8965fb0252c07e3e22f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19066c319c007a43ad914655e2bdb96e",
+    "url": "/xiangqi/index.html"
+  },
+  {
+    "revision": "43d00694bcbf57f45842",
+    "url": "/xiangqi/static/css/main.f99601e0.chunk.css"
+  },
+  {
+    "revision": "8fc900728f95b7e7db8c",
+    "url": "/xiangqi/static/js/2.521f76d6.chunk.js"
+  },
+  {
+    "revision": "43d00694bcbf57f45842",
+    "url": "/xiangqi/static/js/main.a73bfa4e.chunk.js"
+  },
+  {
+    "revision": "4ad3e204c51dabc89519",
+    "url": "/xiangqi/static/js/runtime~main.cf1add30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/xiangqi/static/media/logo.5d5d9eef.svg"
+  }
+]);
